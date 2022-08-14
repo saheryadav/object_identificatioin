6adb915acf5cbdeba0c3d2b6b1956376081c03a0
saheryadav <82718832+saheryadav@users.noreply.github.com>
2022-08-14 12:06:20 +0530

    Add files via upload

--- tree.js
+++ tree.js
@@ -1,5 +1,7 @@
 img3="";
 status="";
+object=[];
+results=[];
 
 function setup(){
     canvas= createCanvas(640, 420);
@@ -21,5 +23,23 @@ function draw(){
 function modelLoaded(){
     console.log("Model Loaded!");
     status="true";
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(img3, gotResult);
+}
+function draw(){
+    if(status !="")
+    {
+        for(i=0; i < object.length; i++)
+        {
+            percent= floor(object[i].confidence * 100);
+            text(object[i].label+"" +percent+"%", object[i].x, object[i].y);
+            rect(object[i].x, object[i].y, object[i].height, object[i].width);
+        }
+    }
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object= results;
 }
